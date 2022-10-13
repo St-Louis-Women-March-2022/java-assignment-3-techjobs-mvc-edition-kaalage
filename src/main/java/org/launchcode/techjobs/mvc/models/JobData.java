ce9dcc25988af7b3083b8e7b7bc4380ee4a919a9
Karina Lage <karinalage@karinas-air.lan>
2022-10-13 15:42:05 -0500

    Completed step 2 - List Views

--- src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
+++ src/main/java/org/launchcode/techjobs/mvc/models/JobData.java
@@ -22,7 +22,7 @@ public class JobData {
     private static final String DATA_FILE = "job_data.csv";
     private static boolean isDataLoaded = false;
 
-    private static ArrayList<Job> allJobs;
+    private static ArrayList<Job> allJobs = new ArrayList<>();
     private static ArrayList<Employer> allEmployers = new ArrayList<>();
     private static ArrayList<Location> allLocations = new ArrayList<>();
     private static ArrayList<PositionType> allPositionTypes = new ArrayList<>();
@@ -205,6 +205,7 @@ public class JobData {
             e.printStackTrace();
         }
     }
+
 
     public static ArrayList<Employer> getAllEmployers() {
         loadData();
